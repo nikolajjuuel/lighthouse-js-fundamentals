e70c4851559723f1d6e292639229af6110aecde8
Nikolaj Johansen <nikolaj.juuel@gmail.com>
2021-08-12 22:09:10 +0000

    which school code

---
+++ lighthouse-js-fundamentals/whichSchool.js
@@ -0,0 +1,18 @@
+// Elementary School if age is below 13
+// Secondary School if age is between 13 and 18 (both inclusive)
+// Lighthouse Labs in all other cases.
+
+
+const whichSchool  = function (age) {
+    // Your code in here ...
+    if(age <= 13){ 
+      return "Elementary School";
+    } else if (age > 13 && age <= 18){
+       return "Secondary School";
+    } else {
+       return "Lighthouse Labs";
+    }
+    
+  }
+  
+  console.log(whichSchool(10));
