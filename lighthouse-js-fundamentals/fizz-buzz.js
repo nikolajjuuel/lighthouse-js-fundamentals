f28d92f6cad2e7a8beabf3c4b3778128ac8f1c4b
Nikolaj Johansen <nikolaj.juuel@gmail.com>
2021-08-12 19:52:01 +0000

    adding files to git

---
+++ lighthouse-js-fundamentals/fizz-buzz.js
@@ -0,0 +1,15 @@
+x = 0 ;
+
+while (x < 11 ){
+   if (x % 5  === 0 && x % 3  === 0 && x > 0){
+        console.log("FizzBuzz"); 
+    } else if (x % 3  === 0 && x > 0) {
+        console.log("Fizz"); 
+    } else if (x % 5  === 0 && x > 0){
+        console.log("Buzz"); 
+    } else {
+        console.log(x);
+          }
+   x++;
+
+}
