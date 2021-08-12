2a6ebb2f85e85824776b7e880a20e74edb3aa565
Nikolaj Johansen <nikolaj.juuel@gmail.com>
2021-08-12 23:21:46 +0000

    adding fizz  buzz

--- lighthouse-js-fundamentals/fizz-buzz.js
+++ lighthouse-js-fundamentals/fizz-buzz.js
@@ -1,15 +1,14 @@
-x = 0 ;
+x = 0;
 
-while (x < 11 ){
-   if (x % 5  === 0 && x % 3  === 0 && x > 0){
-        console.log("FizzBuzz"); 
-    } else if (x % 3  === 0 && x > 0) {
-        console.log("Fizz"); 
-    } else if (x % 5  === 0 && x > 0){
-        console.log("Buzz"); 
-    } else {
-        console.log(x);
-          }
-   x++;
-
+while (x < 11) {
+  if (x % 5 === 0 && x % 3 === 0 && x > 0) {
+    console.log("FizzBuzz");
+  } else if (x % 3 === 0 && x > 0) {
+    console.log("Fizz");
+  } else if (x % 5 === 0 && x > 0) {
+    console.log("Buzz");
+  } else {
+    console.log(x);
+  }
+  x++;
 }
